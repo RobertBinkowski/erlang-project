9d3e761b1375f205eb6e8af4d80185b6f20ad747
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2023-03-19 13:10:51 +0000

    project start

--- Current/project.erl
+++ Current/project.erl
@@ -3,5 +3,18 @@
 %%% @copyright (C) 2022, Robert Binkowski
 %%%-------------------------------------------------------------------
 -module(project).
--include_lib("eunit/include/eunit.hrl").
 -export([start/0]).
+% -export([computeNthPrime/1]).
+%%-include_lib("eunit/include/eunit.hrl").
+
+start() ->
+    io:format("Hello World~n").
+
+% Figure out the nth prime
+% computeNthPrime([])->
+%     [];
+% computeNthPrime([H|T])->
+%     List = lists.filter(fun(N) -> rem H /= 0 end, T);
+%     [H|computeNthPrime(List)];
+% computeNthPrime(N)->
+%     computeNthPrime(Lists: seq(2,N)).
